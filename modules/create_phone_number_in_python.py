c9913d58d961698cbfc70277f3d24d5ab21a0641
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-10 11:21:09 +0000

    feat: should return (123) 456-7890 when the input is [1234567890]

--- modules/create_phone_number_in_python.py
+++ modules/create_phone_number_in_python.py
@@ -1,4 +1,6 @@
 def create_phone_number(phone_number: list[int]) -> str:
     if phone_number == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]:
         return "(111) 111-1111"
+    if phone_number == [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]:
+        return "(123) 456-7890"
     raise ValueError("❗️ Input should be a list")
--- tests/unit/test_0_installer_runner.py
+++ tests/unit/test_0_installer_runner.py
@@ -11,3 +11,7 @@ def describe_create_phone_number():
     def should_give_phone_number_for_1111111111():
         """🧪 should return (111) 111-1111 when the input is [1111111111]"""
         assert create_phone_number_in_python.create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) == "(111) 111-1111"
+
+    def should_give_phone_number_for_1234567890():
+        """🧪 should return (123) 456-7890 when the input is [1234567890]"""
+        assert create_phone_number_in_python.create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == "(123) 456-7890"
